2456c1343e243f265e3ccf2611effbf3626dcc72
EsenbekM <esenbekm03@gmail.com>
2023-03-16 19:59:46 +0600

    lesson 3

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from aiogram.utils import executor
 from config import dp
-from handlers import client, callback, extra, admin
+from handlers import client, callback, extra, admin, fsm_anketa
 import logging
 
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
+fsm_anketa.register_handlers_fsm_anketa(dp)
 
 extra.register_handlers_extra(dp)
 
