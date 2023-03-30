b07fe0ccae95a4dc5440803a785db1ae818e9db0
EsenbekM <esenbekm03@gmail.com>
2023-03-30 18:30:11 +0600

    gitignore

--- parser/films.py
+++ parser/films.py
@@ -1,13 +1,13 @@
-from pprint import pprint
-
 import requests
 from bs4 import BeautifulSoup
 
 URL = "https://rezka.ag/films/"
 
 HEADERS = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,"
+              "image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/108.0.0.0 Safari/537.36"
 }
 
 
